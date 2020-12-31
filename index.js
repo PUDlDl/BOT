c8583abd73ef82353a431de5f07fd1963e7968ae
PUDIDI GANG <76520864+PUDlDl@users.noreply.github.com>
2020-12-31 22:43:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -656,7 +656,7 @@ break
 					teks = (args.length > 1) ? body.slice(8).trim() : ''
 					teks += '\n\n'
 					for (let mem of groupMembers) {
-						teks += `*┣░* @${mem.jid.split('@')[0]}\n`
+						teks += `*✪* @${mem.jid.split('@')[0]}\n`
 						members_id.push(mem.jid)
 					}
 					mentions(teks, members_id, true)
