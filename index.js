f64a37a152a54293bddb9c9379e2e0a3422a3724
Gaje kea Lu xixi <76520864+PUDlDl@users.noreply.github.com>
2021-01-04 08:54:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1672,7 +1672,7 @@ async function starts() {
 						fs.unlinkSync(rano)
 					})
 					break
-				case 'otagall':
+				case 'otagall2':
 					if (!isGroup) return reply(mess.only.group)
 					if (!isGroupAdmins) return reply(mess.only.admin)
 					members_id = []
@@ -1684,14 +1684,14 @@ async function starts() {
 					}
 					mentions(teks, members_id, true)
 					break
-			    case 'otagall2':
+			    case 'otagall3':
 					if (!isGroup) return reply(mess.only.group)
 					if (!isGroupAdmins) return reply(mess.only.admin)
 					members_id = []
 					teks = (args.length > 1) ? body.slice(8).trim() : ''
 					teks += '\n\n'
 					for (let mem of groupMembers) {
-						teks += `*%* wa.me/${mem.jid.split('@')[0]}\n`
+						teks += `*#* wa.me/${mem.jid.split('@')[0]}\n`
 						members_id.push(mem.jid)
 					}
 					mentions(teks, members_id, true)
@@ -1741,12 +1741,12 @@ async function starts() {
 					teks = (args.length > 1) ? body.slice(8).trim() : ''
 					teks += '\n\n'
 					for (let mem of groupMembers) {
-						teks += `*┣ௐ* @${mem.jid.split('@')[0]}\n`
+						teks += `*ௐ* @${mem.jid.split('@')[0]}\n`
 						members_id.push(mem.jid)
 					}
 					mentions(teks, members_id, true)
 					break
-			    case 'otagall3':
+			    case 'otagall':
 					if (!isGroup) return reply(mess.only.group)
 					if (!isGroupAdmins) return reply(mess.only.admin)
 					members_id = []
