e02c6d0fbcd99a95e1391af6a9368c589c17f475
ᜰ 我们是兄 <76520864+PUDlDl@users.noreply.github.com>
2021-01-18 06:58:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] Semua dependensi telah diinstal, jalankan perintah \ "npm start \" untuk segera memulai skrip [ PUDlDl BOT ]"
+echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
