892601b8f4bf0e54b81ad529e299dfbb565c1823
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-17 16:54:39 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,52 +1,215 @@
 const help = (prefix) => {
 	return `AnxietyBot
 	
-	                
-┏━━━°✪〘 INFORMATION 〙✪°━━━┓
-┃
-┣➥ *${prefix}owner*
-┣➥ *${prefix}donasi*
-┣➥ *${prefix}creator*
-┃
-┣━━━°✪〘 MAKER 〙✪°━━━≽
-┃
-┣➥ *${prefix}sticker* [foto]
-┣➥ *${prefix}stickergif* [foto]
-┣➥ *${prefix}sticker nobg 
-┣➥ *${prefix}thunder* [teks]
-┣➥ *${prefix}tsticker* [teks/url]
-┃
-┣━━━━°✪〘 MEDIA 〙✪°━━━≽
-┃
-┣➥ *${prefix}tts* [teks]
-┣➥ *${prefix}ocr*
-┣➥ *${prefix}loli*
-┣➥ *${prefix}toimg*
-┣➥ *${prefix}meme*
-┣➥ *${prefix}memeindo*
-┣➥ *${prefix}nsfwloli*
-┣➥ *${prefix}wait* [foto]
-┣➥ *${prefix}simi
-┣➥ *${prefix}simih*
-┣➥ *${prefix}wait*
-┃
-┣━━━━°✪〘 GROUP 〙✪°━━━━≽
-┃
-┣➥ *${prefix}setname*
-┣➥ *${prefix}setdesc*
-┣➥ *${prefix}getpp*
-┣➥ *${prefix}tagall*
-┣➥ *${prefix}linkgroup
-┣➥ *${prefix}gprofile*
-┣➥ *${prefix}setprefix
-┣➥ *${prefix}welcome*
-┣➥ *${prefix}left*
-┣➥ *${prefix}tagall*
-┃
-┣━━━━°✪〘 END 〙✪°━━━━━≽
-┣━━━━━━━━━━━━━━━━━━━━
-┃ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 AnxietyBot
-┗━━━━━━━━━━━━━━━━━━━━`
+╔══❏〘 INFORMATION 〙❏══╗
+├─❏ *BOT TYPE : JavaScript 
+├─❏ *NAME : Anxiety𝗕𝗢𝗧*
+├─❏ *VERSION  : 4.2*
+╰──────────────────	                
+
+*ABOUT BOT*
+──────────────────
+❏ *${prefix}owner*
+❏ *${prefix}donasi*
+❏ *${prefix}info*
+❏ *${prefix}snk*
+❏ *${prefix}report*
+❏ *${prefix}request*
+╰──────────────────
+
+*MAKER*
+──────────────────
+❏ *${prefix}ocr*
+❏ *${prefix}sticker*
+❏ *${prefix}gtts*
+❏ *${prefix}toimg*
+╰──────────────────
+
+*LOGO MAKER*
+──────────────────
+❏ *${prefix}phlogo*
+❏ *${prefix}text3d*
+❏ *${prefix}thunder*
+❏ *${prefix}bplogo*
+╰──────────────────
+
+*Random Picture Meme*
+──────────────────
+❏ *${prefix}meme*
+❏ *${prefix}memeindo*
+╰──────────────────
+
+*Random Picture Anime*
+──────────────────
+❏ *${prefix}waifu*
+❏ *${prefix}neko*
+❏ *${prefix}loli*
+❏ *${prefix}nime*
+❏ *${prefix}osakana*
+❏ *${prefix}naruto*
+❏ *${prefix}animegirl*
+❏ *${prefix}animeboy*
+╰──────────────────
+
+*Random Picture Bembem*
+──────────────────
+❏ *${prefix}cars*
+❏ *${prefix}motor*
+╰──────────────────
+
+*Random Picture AlatTakol* 
+──────────────────
+❏ *${prefix}gitar*
+❏ *${prefix}drum*
+╰──────────────────
+
+*Random Pict Cwk* 
+╰──────────────────
+❏ *${prefix}cewek*
+❏ *${prefix}cowok*
+╰───────────────────
+
+*Random Pict CsPlayJPN* 
+──────────────────
+❏ *${prefix}mano*
+❏ *${prefix}aliga*
+❏ *${prefix}liyuu*
+❏ *${prefix}miluki*
+❏ *${prefix}rikucos*
+❏ *${prefix}tieunhu*
+❏ *${prefix}arurinh*
+❏ *${prefix}kmainemu*
+❏ *${prefix}realyami*
+❏ *${prefix}sakuragun*
+❏ *${prefix}haodadedouya*
+❏ *${prefix}meruhanemutai*
+❏ *${prefix}cosplayjapan*
+╰──────────────────
+
+*Random Pict Animasi* 
+──────────────────
+❏ *${prefix}pokemon*
+❏ *${prefix}doraemon*
+╰──────────────────
+
+*Random Pict BTS* 
+──────────────────
+❏ *${prefix}bts*
+❏ *${prefix}jimin*
+❏ *${prefix}minyoongi*
+❏ *${prefix}kimnamjoon*
+❏ *${prefix}kimseokjin*
+❏ *${prefix}junghoseok*
+❏ *${prefix}kimtaehyung*
+❏ *${prefix}jeonjungkook*
+╰──────────────────
+
+*Random Pict B.Pink* 
+──────────────────
+❏ *${prefix}blackpink*
+❏ *${prefix}lisa*
+❏ *${prefix}jennie*
+❏ *${prefix}kimjisoo*
+❏ *${prefix}roseannepark*
+╰──────────────────
+
+*Random Pict Animals*
+──────────────────
+❏ *${prefix}anjing*
+❏ *${prefix}kucing*
+❏ *${prefix}hamster*
+╰──────────────────
+
+*Random Pict Quotes*
+──────────────────
+❏ *${prefix}katabijak*
+❏ *${prefix}katakata*
+❏ *${prefix}quotesid*
+❏ *${prefix}quotesen*
+❏ *${prefix}quotesislami*
+╰──────────────────
+
+*Random Pict Estetik*
+──────────────────
+❏ *${prefix}aesthetic*
+❏ *${prefix}bluesky*
+❏ *${prefix}flower*
+╰──────────────────
+
+*KERANG*
+──────────────────
+❏ *${prefix}kapankah*
+❏ *${prefix}apakah*
+❏ *${prefix}bisakah*
+❏ *${prefix}rate*
+╰──────────────────
+
+*DOWNLOADER*
+──────────────────
+❏ *${prefix}ytmp4*
+❏ *${prefix}ytmp3*
+╰──────────────────
+
+*OTHERS*
+❏ *${prefix}fototiktok*
+❏ *${prefix}map*
+❏ *${prefix}kbbi*
+❏ *${prefix}artinama*
+❏ *${prefix}infogempa*
+❏ *${prefix}wiki*
+❏ *${prefix}wikien*
+❏ *${prefix}bucin*
+❏ *${prefix}igstalk*
+❏ *${prefix}persengay*
+❏ *${prefix}quotes*
+❏ *${prefix}pantun*
+❏ *${prefix}faktaunik*
+❏ *${prefix}katacinta*
+❏ *${prefix}katabijak*
+❏ *${prefix}truth*
+❏ *${prefix}dare*
+❏ *${prefix}lirik*
+❏ *${prefix}chord*
+❏ *${prefix}gcainebot*
+❏ *${prefix}hidetag*
+❏ *${prefix}ssweb*
+❏ *${prefix}indohot*
+❏ *${prefix}pesankosong*
+╰──────────────────
+
+*GROUP*
+ᜰ➢Admin Only
+──────────────────
+❏ *${prefix}linkgc*
+❏ *${prefix}tagall*
+❏ *${prefix}otagall*
+❏ *${prefix}mentionall*
+❏ *${prefix}tagme*
+❏ *${prefix}add* [+62xx]
+❏ *${prefix}group* [buka/tutup]
+❏ *${prefix}demote* [@tag]
+❏ *${prefix}promote* [@tag]
+❏ *${prefix}kick* [@tag]
+❏ *${prefix}listadmin*
+❏ *${prefix}simih* [1/0]
+❏ *${prefix}nsfw* [1/0] 
+❏ *${prefix}welcome* [1/0]
+╰──────────────────
+
+*OWNER BOT*
+──────────────────
+❏ *${prefix}clearall*
+❏ *${prefix}blocklist*
+❏ *${prefix}block*
+❏ *${prefix}unblock*
+❏ *${prefix}bc*
+❏ *${prefix}bc1*
+❏ *${prefix}bc2*
+❏ *${prefix}leave*
+❏ *${prefix}setpp*
+❏ *${prefix}setprefix*
+╰──────────────────
+`
 }
 
 exports.help = help
