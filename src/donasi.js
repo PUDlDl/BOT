b8751d4aa60c17ee0f3df903a767af5f74a7e996
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-17 17:03:22 +0700

    Update donasi.js

--- src/donasi.js
+++ src/donasi.js
@@ -1,18 +1,14 @@
 const donasi = () => {
-	return `PUDIDIBOT	
+	return `	
 
-   ═━━━━✪〘 DONASI 〙✪━━━━═
-┏━━━━━━━━━━━━━━━━━━━━━━
-┃Haee. *${id.split("@s.whatsapp.net")[0]}* 👋️    
-┣━━━━━━━━━━━━━━━━━━━━━━
-┃╭──❉ DONASI SEIKHLASNYA ❉
-┃│➤ PULSA : 0877-1474-5440
-┃│➤ DANA  : 0877-1474-5440
-┃│➤ OVO   : 0877-1474-5440
-┃╰────────────────────❉ 
-┣━━━━━━━━━━━━━━━━━━━━━━
-┃TERIMAKASIH SUDAH DONASIツ
-┗━━━━━━━━━━━━━━━━━━━━━━`
+Heyow. *${id.split("@s.whatsapp.net")[0]}* 👋️
+
+╔══❀ 〘 DONASI 〙 ❀══╗
+├─❏ SAWERIA : saweria.co/PUDlDl
+├─❏ PULSA : 0877-1474-5440
+├─❏ DANA : 0877-1474-5440
+├─❏ OVO : 0877-1474-5440
+╰──────────────────`
 }
 
 exports.donasi = donasi
